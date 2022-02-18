dae4cb369f3d19dc9b0ae4c94afc2fab46f9d76e
Coal <dibashtori@gmail.com>
2022-02-17 23:11:47 -0600

    Remove src/library to switch to a git submodule

--- src/library/lpm/toml_lib.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#define TOML_HEADER_ONLY 0
-#define TOML_ENABLE_FORMATTERS 0
-#include "toml.hpp"
--- src/library/lpm/types.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <map>
-#include <vector>
-#include <string>
-
-// A map of strings to strings.
-typedef std::map<std::string, std::string> args_t;
-typedef std::map<std::string, std::vector<std::string>> args_map_t;
